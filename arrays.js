b771902773f94fe627123cd46f9b483367ace21c
kalex19 <klewis@rockymountainprep.org>
2018-06-27 01:39:27 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,4 @@
+var grilledCheeseIngredients = ['bread', 'mild cheese', 'sharp cheese', 'butter', 'tomato', 'garlic']
+var tomatoSauceIngredients = ['tomato', 'garlic', 'olive oil', 'basil' 'oregano'] 
+var evenNumbers = new Array (); 
+var chacolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'] 
