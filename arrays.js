e08ff3faa3b6ec01151f1c1976c95a6e00e47e3d
kalex19 <klewis@rockymountainprep.org>
2018-06-27 01:43:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var grilledCheeseIngredients = ['bread', 'mild cheese', 'sharp cheese', 'butter', 'tomato', 'garlic']
 var tomatoSauceIngredients = ['tomato', 'garlic', 'olive oil', 'basil' 'oregano'] 
 var evenNumbers = new Array (); 
-var chacolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'] 
+var chacolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'] function addElementToBeginningOfArray = ();
+function destructivelyAddElementToBeginningOfArray = ();
